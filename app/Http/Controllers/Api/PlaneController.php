0fdacb2cac402457a72d3780544958079c4a906a
TOKIO-V2 <albertohernandezgaleote6@gmail.com>
2025-02-24 15:45:24 +0100

    feat:Added Planecontroller

---
+++ app/Http/Controllers/Api/PlaneController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\planeModel;
+use Illuminate\Http\Request;
+
+class PlaneController extends Controller
+{
+    public function index()
+    {
+        return planeModel::all();
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'max_capacity' => 'required|integer',
+        ]);
+
+        return planeModel::create($request->all());
+    }
+
+    public function show(planeModel $plane)
+    {
+        return $plane;
+    }
+
+    public function update(Request $request, planeModel $plane)
+    {
+        $plane->update($request->all());
+        return $plane;
+    }
+
+    public function destroy(planeModel $plane)
+    {
+        $plane->delete();
+        return response()->noContent();
+    }
+}
