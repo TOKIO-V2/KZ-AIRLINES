389a4198bf706f8034c181cc67f4687e2bf9114b
TOKIO-V2 <albertohernandezgaleote6@gmail.com>
2025-02-25 12:18:23 +0100

    Fix: Naming of the routes.

--- routes/web.php
+++ routes/web.php
@@ -5,24 +5,24 @@ use App\Http\Controllers\PlaneController;
 use App\Http\Controllers\FlightController;
 
 
-Route::get('/', [FlightController::class, 'index'])->name('flights.index');
-Route::get('/flights/past', [FlightController::class, 'pastFlights'])->name('flights.past');
-Route::get('/flights/{id}', [FlightController::class, 'show'])->name('flights.show');
+Route::get('/', [FlightController::class, 'index'])->name('flightsIndex');
+Route::get('/flights/past', [FlightController::class, 'pastFlights'])->name('flightsPast');
+Route::get('/flights/{id}', [FlightController::class, 'show'])->name('flightsShow');
 Route::middleware(['auth', 'role:admin'])->group(function () {
-Route::get('/flights/create', [FlightController::class, 'create'])->name('flights.create');
-Route::post('/flights/store', [FlightController::class, 'store'])->name('flights.store');
-Route::get('/flights/{id}/edit', [FlightController::class, 'edit'])->name('flights.edit');
-Route::put('/flights/update/{id}', [FlightController::class, 'update'])->name('flights.update');
-Route::delete('/flights/destroy/{id}', [FlightController::class, 'destroy'])->name('flights.destroy');
+Route::get('/flights/create', [FlightController::class, 'create'])->name('flightsCreate');
+Route::post('/flights/store', [FlightController::class, 'store'])->name('flightsStore');
+Route::get('/flights/{id}/edit', [FlightController::class, 'edit'])->name('flightsEdit');
+Route::put('/flights/update/{id}', [FlightController::class, 'update'])->name('flightsUpdate');
+Route::delete('/flights/destroy/{id}', [FlightController::class, 'destroy'])->name('flightsDestroy');
 });
 
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
-    Route::get('/planes', [PlaneController::class, 'index'])->name('planes.index');
-    Route::get('/planes/create', [PlaneController::class, 'create'])->name('planes.create');
-    Route::post('/planes/store', [PlaneController::class, 'store'])->name('planes.store');
-    Route::get('/planes/{id}', [PlaneController::class, 'show'])->name('planes.show');
-    Route::get('/planes/{id}/edit', [PlaneController::class, 'edit'])->name('planes.edit');
-    Route::put('/planes/update/{id}', [PlaneController::class, 'update'])->name('planes.update');
-    Route::delete('/planes/destroy/{id}', [PlaneController::class, 'destroy'])->name('planes.destroy');
+    Route::get('/planes', [PlaneController::class, 'index'])->name('planesIndex');
+    Route::get('/planes/create', [PlaneController::class, 'create'])->name('planesCreate');
+    Route::post('/planes/store', [PlaneController::class, 'store'])->name('planesStore');
+    Route::get('/planes/{id}', [PlaneController::class, 'show'])->name('planesShow');
+    Route::get('/planes/{id}/edit', [PlaneController::class, 'edit'])->name('planesEdit');
+    Route::put('/planes/update/{id}', [PlaneController::class, 'update'])->name('planesUpdate');
+    Route::delete('/planes/destroy/{id}', [PlaneController::class, 'destroy'])->name('planesDestroy');
 });
